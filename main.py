b6b31486335cc6ca70821004aeb1886517d97587
JiYu <eunjun1018@gmail.com>
2025-07-02 19:35:35 +0900

    코디세이 과정1-문제1 풀이 보완

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import csv
 
-file_path = 'mission_computer_main.log'
+origin_file = 'mission_computer_main.log'
 output_file = 'log_analysis.md'
 
 try:
   # mission_computer_main.log 파일 읽기
-  with open(file_path, 'r', encoding='utf-8') as file:
+  with open(origin_file, 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     header = next(reader)
     rows = list(reader)
@@ -17,9 +17,16 @@ try:
     with open(output_file, 'w', encoding='utf-8') as md_file:
       md_file.write('[log_analysis 보고서]\n\n')
       for row in rows:
-            md_file.write(f'| {row[0]} | {row[1]} | {row[2]}\n')
+          md_file.write(f'| {row[0]} | {row[1]} | {row[2]}\n')
 
     print(f'{output_file} 파일이 생성되었습니다.')
 
 except FileNotFoundError:
-  print(f'Error: {file_path} 파일을 찾을 수 없습니다.')
+  print(f'에러내용 : {origin_file} 파일을 찾을 수 없습니다.')
+except Exception as e:
+  print(f'에러내용 : {e}')
+
+
+# csv? 데이터를 쉼표 혹은 다른 구분자로 구분하여 저장하는 텍스트 파일 형식. (comma-separated values)
+# csv.reader : csv 파일의 데이터를 row(행) 단위로 읽는다.
+# next(reader) : reader에서 첫번째 행을 가져온다.
