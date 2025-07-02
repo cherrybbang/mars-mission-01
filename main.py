8e349930767584ba5a2443f89718eb47f3e8d7ce
JiYu <eunjun1018@gmail.com>
2025-07-02 17:45:24 +0900

    코디세이 과정1-문제1 풀이

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+
+file_path = 'mission_computer_main.log'
+output_file = 'log_analysis.md'
+
+try:
+  # mission_computer_main.log 파일 읽기
+  with open(file_path, 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    rows = list(reader)
+
+    # 시간역순으로 정렬
+    rows.sort(key=lambda x: x[0], reverse=True)
+
+    # 마크다운으로 저장
+    with open(output_file, 'w', encoding='utf-8') as md_file:
+      md_file.write('[log_analysis 보고서]\n\n')
+      for row in rows:
+            md_file.write(f'| {row[0]} | {row[1]} | {row[2]}\n')
+
+    print(f'{output_file} 파일이 생성되었습니다.')
+
+except FileNotFoundError:
+  print(f'Error: {file_path} 파일을 찾을 수 없습니다.')
