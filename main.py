7d750a4d699adc168cc222cd5af426601e4ce126
JiYu <eunjun1018@gmail.com>
2025-07-02 19:59:28 +0900

    코디세이 과정1-문제2 풀이

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import csv
+import json
 
 origin_file = 'mission_computer_main.log'
-output_file = 'log_analysis.md'
+output_md_file = 'log_analysis.md'
+output_json_file = 'mission_computer_main.json'
 
 try:
   # mission_computer_main.log 파일 읽기
@@ -13,13 +15,20 @@ try:
     # 시간역순으로 정렬
     rows.sort(key=lambda x: x[0], reverse=True)
 
+    # 리스트 데이터를 dict로 변환
+    log_dicts = [dict(zip(header, row)) for row in rows]
+
     # 마크다운으로 저장
-    with open(output_file, 'w', encoding='utf-8') as md_file:
+    with open(output_md_file, 'w', encoding='utf-8') as md_file:
       md_file.write('[log_analysis 보고서]\n\n')
       for row in rows:
           md_file.write(f'| {row[0]} | {row[1]} | {row[2]}\n')
 
-    print(f'{output_file} 파일이 생성되었습니다.')
+    # JSON으로 저장
+    with open(output_json_file, 'w', encoding='utf-8') as json_file:
+      json.dump(log_dicts, json_file, ensure_ascii=False, indent=4)
+
+    print(f'{output_md_file} 파일과 {output_json_file} 파일이 생성되었습니다.')
 
 except FileNotFoundError:
   print(f'에러내용 : {origin_file} 파일을 찾을 수 없습니다.')
